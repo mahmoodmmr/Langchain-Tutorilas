b2dc8a6084adbd873add3619fe2adedc49881508
ashke98 <ashke981@gmail.com>
2023-12-24 18:04:40 +0330

    mahmood commiting from ashkans laptop: converting gpt memory to flask api

---
+++ Chatbots-with-memory/chat_api_flask.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, jsonify
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+llm_name = "gpt-3.5-turbo"
+
+app = Flask(__name__)
+
+
+class ChatWithYourDataBot:
+    def __init__(self):
+        self.loaded_file = (
+            "G:\\Desktop\\Simple Sample\\AiHealth\\Job 4\\nutrirtion_nemone_Diet.pdf"
+        )
+        self.qa = self.load_db(self.loaded_file, "stuff", 4)
+
+    def load_db(self, file, chain_type, k):
+        loader = PyPDFLoader(file)
+        documents = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=150
+        )
+        docs = text_splitter.split_documents(documents)
+
+        embeddings = OpenAIEmbeddings()
+        db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+        retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
+
+        qa = ConversationalRetrievalChain.from_llm(
+            llm=ChatOpenAI(model_name=llm_name, temperature=0),
+            chain_type=chain_type,
+            retriever=retriever,
+            return_source_documents=True,
+            return_generated_question=True,
+        )
+        return qa
+
+    def _convert_document_to_dict(self, document):
+        # This is a generic method; adjust based on your Document class structure
+        document_dict = {}
+
+        # Assuming Document has a 'metadata' attribute
+        document_dict["metadata"] = document.metadata
+
+        # Assuming Document has a 'get_content()' method
+        if hasattr(document, "get_content"):
+            document_dict["content"] = document.get_content()
+        # If not, you might need to adjust this based on the actual structure
+
+        # Add other attributes or methods as needed
+
+        return document_dict
+
+    def ask_question(self, query):
+        result = self.qa({"question": query, "chat_history": []})
+        # Convert Document objects to JSON-serializable format
+        result["source_documents"] = [
+            self._convert_document_to_dict(doc) for doc in result["source_documents"]
+        ]
+        return result
+
+
+chat_bot = ChatWithYourDataBot()
+
+
+@app.route("/ask", methods=["POST"])
+def ask_question_api():
+    data = request.get_json()
+    query = data.get("question")
+    response = chat_bot.ask_question(query)
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Chatbots-with-memory/chatbot.py
+++ Chatbots-with-memory/chatbot.py
@@ -1,8 +1,11 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.document_loaders import TextLoader
-from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
@@ -10,9 +13,9 @@ from langchain.document_loaders import PyPDFLoader
 
 import os
 
-os.environ["OPENAI_API_KEY"] = "**************"
-
+os.environ["OPENAI_API_KEY"] = "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"
 llm_name = "gpt-3.5-turbo"
+
 
 def load_db(file, chain_type, k):
     # load documents
@@ -31,17 +34,17 @@ def load_db(file, chain_type, k):
 
     # define retriever
     retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
-    
+
     # create a chatbot chain. Memory is managed externally.
     qa = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model_name=llm_name, temperature=0), 
-        chain_type=chain_type, 
-        retriever=retriever, 
+        llm=ChatOpenAI(model_name=llm_name, temperature=0),
+        chain_type=chain_type,
+        retriever=retriever,
         return_source_documents=True,
         return_generated_question=True,
     )
-    
-    return qa 
+
+    return qa
 
 
 import panel as pn
@@ -53,115 +56,159 @@ pn.extension()
 class cbfs(param.Parameterized):
     chat_history = param.List([])
     answer = param.String("")
-    db_query  = param.String("")
+    db_query = param.String("")
     db_response = param.List([])
-    
-    def __init__(self,  **params):
-        super(cbfs, self).__init__( **params)
+
+    def __init__(self, **params):
+        super(cbfs, self).__init__(**params)
         self.panels = []
-        self.loaded_file = "E:\\Dev\\5_main_project\\haoma-ai\\backend\\ai.service\\data\\Diet & Nutrition.pdf"
-        self.qa = load_db(self.loaded_file,"stuff", 4)
-    
+        self.loaded_file = (
+            "G:\\Desktop\\Simple Sample\\AiHealth\\Job 4\\nutrirtion_nemone_Diet.pdf"
+        )
+        self.qa = load_db(self.loaded_file, "stuff", 4)
+
     def call_load_db(self, count):
         if count == 0 or file_input.value is None:  # init or no file specified :
             return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
         else:
             file_input.save("temp.pdf")  # local copy
             self.loaded_file = file_input.filename
-            button_load.button_style="outline"
+            button_load.button_style = "outline"
             self.qa = load_db("temp.pdf", "stuff", 4)
-            button_load.button_style="solid"
+            button_load.button_style = "solid"
         self.clr_history()
         return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
 
     def convchain(self, query):
         if not query:
-            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+            return pn.WidgetBox(
+                pn.Row("User:", pn.pane.Markdown("", width=600)), scroll=True
+            )
         result = self.qa({"question": query, "chat_history": self.chat_history})
         self.chat_history.extend([(query, result["answer"])])
         self.db_query = result["generated_question"]
         self.db_response = result["source_documents"]
-        self.answer = result['answer'] 
-        self.panels.extend([
-            pn.Row('User:', pn.pane.Markdown(query, width=600)),
-            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, style={'background-color': '#F6F6F6'}))
-        ])
-        print('ChatBot:' + self.answer)
-        inp.value = ''  #clears loading indicator when cleared
-        return pn.WidgetBox(*self.panels,scroll=True)
+        self.answer = result["answer"]
+        self.panels.extend(
+            [
+                pn.Row("User:", pn.pane.Markdown(query, width=600)),
+                pn.Row(
+                    "ChatBot:",
+                    pn.pane.Markdown(
+                        self.answer, width=600, style={"background-color": "#F6F6F6"}
+                    ),
+                ),
+            ]
+        )
+        print("ChatBot:" + self.answer)
+        inp.value = ""  # clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels, scroll=True)
 
-    @param.depends('db_query ', )
+    @param.depends(
+        "db_query ",
+    )
     def get_lquest(self):
-        if not self.db_query :
+        if not self.db_query:
             return pn.Column(
-                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
-                pn.Row(pn.pane.Str("no DB accesses so far"))
+                pn.Row(
+                    pn.pane.Markdown(
+                        f"Last question to DB:", styles={"background-color": "#F6F6F6"}
+                    )
+                ),
+                pn.Row(pn.pane.Str("no DB accesses so far")),
             )
         return pn.Column(
-            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
-            pn.pane.Str(self.db_query )
+            pn.Row(
+                pn.pane.Markdown(f"DB query:", styles={"background-color": "#F6F6F6"})
+            ),
+            pn.pane.Str(self.db_query),
         )
 
-    @param.depends('db_response', )
+    @param.depends(
+        "db_response",
+    )
     def get_sources(self):
         if not self.db_response:
-            return 
-        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
+            return
+        rlist = [
+            pn.Row(
+                pn.pane.Markdown(
+                    f"Result of DB lookup:", styles={"background-color": "#F6F6F6"}
+                )
+            )
+        ]
         for doc in self.db_response:
             rlist.append(pn.Row(pn.pane.Str(doc)))
         return pn.WidgetBox(*rlist, width=600, scroll=True)
 
-    @param.depends('convchain', 'clr_history') 
+    @param.depends("convchain", "clr_history")
     def get_chats(self):
         if not self.chat_history:
-            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True)
-        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
+            return pn.WidgetBox(
+                pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True
+            )
+        rlist = [
+            pn.Row(
+                pn.pane.Markdown(
+                    f"Current Chat History variable",
+                    styles={"background-color": "#F6F6F6"},
+                )
+            )
+        ]
         for exchange in self.chat_history:
             rlist.append(pn.Row(pn.pane.Str(exchange)))
         return pn.WidgetBox(*rlist, width=600, scroll=True)
 
-    def clr_history(self,count=0):
+    def clr_history(self, count=0):
         self.chat_history = []
-        return 
+        return
 
 
 cb = cbfs()
 
-file_input = pn.widgets.FileInput(accept='.pdf')
-button_load = pn.widgets.Button(name="Load DB", button_type='primary')
-button_clearhistory = pn.widgets.Button(name="Clear History", button_type='warning')
+file_input = pn.widgets.FileInput(accept=".pdf")
+button_load = pn.widgets.Button(name="Load DB", button_type="primary")
+button_clearhistory = pn.widgets.Button(name="Clear History", button_type="warning")
 button_clearhistory.on_click(cb.clr_history)
-inp = pn.widgets.TextInput( placeholder='Enter text here…')
+inp = pn.widgets.TextInput(placeholder="Enter text here…")
 
 bound_button_load = pn.bind(cb.call_load_db, button_load.param.clicks)
-conversation = pn.bind(cb.convchain, inp) 
+conversation = pn.bind(cb.convchain, inp)
 
-jpg_pane = pn.pane.Image( './img/convchain.jpg')
+jpg_pane = pn.pane.Image("./img/convchain.jpg")
 
 tab1 = pn.Column(
     pn.Row(inp),
     pn.layout.Divider(),
-    pn.panel(conversation,  loading_indicator=True, height=300),
+    pn.panel(conversation, loading_indicator=True, height=300),
     pn.layout.Divider(),
 )
-tab2= pn.Column(
+tab2 = pn.Column(
     pn.panel(cb.get_lquest),
     pn.layout.Divider(),
-    pn.panel(cb.get_sources ),
+    pn.panel(cb.get_sources),
 )
-tab3= pn.Column(
+tab3 = pn.Column(
     pn.panel(cb.get_chats),
     pn.layout.Divider(),
 )
-tab4=pn.Column(
-    pn.Row( file_input, button_load, bound_button_load),
-    pn.Row( button_clearhistory, pn.pane.Markdown("Clears chat history. Can use to start a new topic" )),
+tab4 = pn.Column(
+    pn.Row(file_input, button_load, bound_button_load),
+    pn.Row(
+        button_clearhistory,
+        pn.pane.Markdown("Clears chat history. Can use to start a new topic"),
+    ),
     pn.layout.Divider(),
-    pn.Row(jpg_pane.clone(width=400))
+    pn.Row(jpg_pane.clone(width=400)),
 )
 dashboard = pn.Column(
-    pn.Row(pn.pane.Markdown('# ChatWithYourData_Bot')),
-    pn.Tabs(('Conversation', tab1), ('Database', tab2), ('Chat History', tab3),('Configure', tab4))
+    pn.Row(pn.pane.Markdown("# ChatWithYourData_Bot")),
+    pn.Tabs(
+        ("Conversation", tab1),
+        ("Database", tab2),
+        ("Chat History", tab3),
+        ("Configure", tab4),
+    ),
 )
 
 
